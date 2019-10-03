0fe99d0a72047303a61c8b05f0b4dc0a05418bbc
Peitong Liu <vincentliu9816@gmail.com>
2019-10-02 20:03:12 -0400

    Repo added

---
+++ Assets/_Scripts/SkyController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour
+{
+    public float horizontalSpeed = 0.1f;
+    public float resetPosition = 4.5f;
+    public float resetPoint = -4.5f;
+
+    // Start is called before the first frame update.
+    void Start()
+    {
+        //Reset();
+    }
+    // Update is called once per frame.
+    void Update()
+    {
+        Move();
+        BoundaryCheck();
+    }    
+    //This method moves the sky to left by horizontalSpeed.
+    void Move()
+    {
+        Vector2 newPosition = new Vector2(horizontalSpeed, 0.0f);
+        Vector2 currentPosition = transform.position;
+        currentPosition -= newPosition;
+        transform.position = currentPosition;
+    }
+    //This method resets the position of the sky.
+    void Reset()
+    {
+        transform.position = new Vector2(resetPosition, 0.0f);
+    }
+    //This method checks if the sky meets the reset point.
+    void BoundaryCheck()
+    {
+        if (transform.position.x <= resetPoint)
+        {
+            Reset();
+        }
+    }
+}
+
+
+
